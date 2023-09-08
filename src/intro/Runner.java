6e9c86a4f20b871fe64642bf777d7400fd1eac22
br-watson <bradleyrwatson@hotmail.com>
2023-09-08 10:20:48 +0100

    Changed main

--- src/intro/Runner.java
+++ src/intro/Runner.java
@@ -2,6 +2,6 @@ package intro;
 
 public class Runner {
     public static void main(String[] args){
-        System.out.println("hello, world!");
+        System.out.println("Hello, world!");
     }
 }
