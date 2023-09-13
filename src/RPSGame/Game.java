f3769dcc90045bb7c5f61f2ba7df094cb44f10c5
br-watson <bradleyrwatson@hotmail.com>
2023-09-13 16:23:40 +0100

    Changed game

--- src/RPSGame/Game.java
+++ src/RPSGame/Game.java
@@ -5,26 +5,10 @@ import java.util.Random;
 public class Game {
     private Random rand = new Random();
 
-    public Game() {
-
-    }
-
-    public String RPS(RPS input) {
+    public String RPSGame(RPS input) {
         Boolean result = null;
-        String output;
         int num = rand.nextInt(3);
-        RPS computer = null;
-        switch (num) {
-            case 0:
-                computer = RPS.ROCK;
-                break;
-            case 1:
-                computer = RPS.PAPER;
-                break;
-            case 2:
-                computer = RPS.SCISSORS;
-                break;
-        }
+        RPS computer = RPS.values()[num];
         if (input != computer) {
             switch (input) {
                 case ROCK:
@@ -35,16 +19,13 @@ public class Game {
                     break;
                 case SCISSORS:
                     result = computer != RPS.ROCK;
-                    break;
             }
         }
         if (result == null)
-            output = "Tie. :|";
+            return "Tie. :|";
         else if (result)
-            output = "You Win! :)";
+            return "You Win! :)";
         else
-            output = "You Lose :(";
-
-        return output;
+            return "You Lose :(";
     }
 }
--- src/RPSGame/Runner.java
+++ src/RPSGame/Runner.java
@@ -4,6 +4,6 @@ public class Runner {
     public static void main(String[] args) {
         Game game = new Game();
         for (int i = 0; i < 5; i++)
-            System.out.println(game.RPS(RPS.ROCK));
+            System.out.println(game.RPSGame(RPS.ROCK));
     }
 }
